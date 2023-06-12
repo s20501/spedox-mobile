16bb5ef08d4888eb8f6fc944cd30e0c86fe053f4
s20501 <s20501@pjwstk.edu.pl>
2023-06-12 20:52:14 +0200

    Document model

--- app/src/main/java/com/example/spedox_mobile/models/DocumentRestModel.java
+++ app/src/main/java/com/example/spedox_mobile/models/DocumentRestModel.java
@@ -1,64 +1,128 @@
 package com.example.spedox_mobile.models;
 
-
 import java.io.File;
 
+/**
+ * Represents a REST model for a document.
+ * This class holds information about a document rest model.
+ */
 public class DocumentRestModel {
     private String id;
     private String type;
-  //  private DocumentStatusEnum status;
     private String clientId;
     private String shipmentId;
-//    private Date addedAt;
-//    private Date updatedAt;
     private String creator;
     private File file;
 
+    /**
+     * Sets the type of the document.
+     *
+     * @param type The type of the document.
+     */
     public void setType(String type) {
         this.type = type;
     }
 
-
+    /**
+     * Sets the client ID associated with the document.
+     *
+     * @param clientId The client ID associated with the document.
+     */
     public void setClientId(String clientId) {
         this.clientId = clientId;
     }
 
+    /**
+     * Sets the shipment ID associated with the document.
+     *
+     * @param shipmentId The shipment ID associated with the document.
+     */
     public void setShipmentId(String shipmentId) {
         this.shipmentId = shipmentId;
     }
 
+    /**
+     * Sets the creator of the document.
+     *
+     * @param creator The creator of the document.
+     */
     public void setCreator(String creator) {
         this.creator = creator;
     }
 
+    /**
+     * Sets the file of the document.
+     *
+     * @param file The file of the document.
+     */
     public void setFile(File file) {
         this.file = file;
     }
 
+    /**
+     * Gets the ID of the document.
+     *
+     * @return The ID of the document.
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Gets the type of the document.
+     *
+     * @return The type of the document.
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * Gets the client ID associated with the document.
+     *
+     * @return The client ID associated with the document.
+     */
     public String getClientId() {
         return clientId;
     }
 
+    /**
+     * Gets the shipment ID associated with the document.
+     *
+     * @return The shipment ID associated with the document.
+     */
     public String getShipmentId() {
         return shipmentId;
     }
 
+    /**
+     * Gets the creator of the document.
+     *
+     * @return The creator of the document.
+     */
     public String getCreator() {
         return creator;
     }
 
+    /**
+     * Gets the file of the document.
+     *
+     * @return The file of the document.
+     */
     public File getFile() {
         return file;
     }
 
+    /**
+     * Constructs a new DocumentRestModel object with the specified ID, type, client ID, shipment ID, creator, and file.
+     *
+     * @param id         The ID of the document.
+     * @param type       The type of the document.
+     * @param clientId   The client ID associated with the document.
+     * @param shipmentId The shipment ID associated with the document.
+     * @param creator    The creator of the document.
+     * @param file       The file of the document.
+     */
     public DocumentRestModel(String id, String type, String clientId, String shipmentId, String creator, File file) {
         this.id = id;
         this.type = type;
@@ -68,6 +132,14 @@ public class DocumentRestModel {
         this.file = file;
     }
 
+    /**
+     * Constructs a new DocumentRestModel object with the specified ID, type, shipment ID, and file.
+     *
+     * @param id         The ID of the document.
+     * @param type       The type of the document.
+     * @param shipmentId The shipment ID associated with the document.
+     * @param file       The file of the document.
+     */
     public DocumentRestModel(String id, String type, String shipmentId, File file) {
         this.id = id;
         this.type = type;
@@ -75,6 +147,9 @@ public class DocumentRestModel {
         this.file = file;
     }
 
-    public DocumentRestModel(){
+    /**
+     * Constructs a new empty DocumentRestModel object.
+     */
+    public DocumentRestModel() {
     }
 }
--- app/src/main/java/com/example/spedox_mobile/models/LoginModel.java
+++ app/src/main/java/com/example/spedox_mobile/models/LoginModel.java
@@ -1,27 +1,56 @@
 package com.example.spedox_mobile.models;
 
+/**
+ * Represents a login model.
+ * This class holds information about a user's login form.
+ */
 public class LoginModel {
-
     private String userName;
     private String password;
 
+    /**
+     * Constructs a new LoginModel object with the specified username and password.
+     *
+     * @param userName The username for the login.
+     * @param password The password for the login.
+     */
     public LoginModel(String userName, String password) {
         this.userName = userName;
         this.password = password;
     }
 
+    /**
+     * Gets the username for the login.
+     *
+     * @return The username for the login.
+     */
     public String getUserName() {
         return userName;
     }
 
+    /**
+     * Sets the username for the login.
+     *
+     * @param userName The username for the login.
+     */
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
+    /**
+     * Gets the password for the login.
+     *
+     * @return The password for the login.
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Sets the password for the login.
+     *
+     * @param password The password for the login.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
